7b266534706b6343d17edeba35e7dc3c85ba2e52
DefectingCat <i@defect.ink>
2022-09-21 11:50:52 +0800

    add globe earth

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
-import useThree from 'lib/hooks/useThree';
+import useThree, { InitFn, THREE } from 'lib/hooks/useThree';
+import world from 'assets/world.jpg';
 
-const initThree = () => {};
+const loader = new THREE.TextureLoader();
+const initThree: InitFn = (three) => {
+  const { scene, camera, controls } = three;
+  camera.position.set(0, 8, 3);
+
+  {
+    const light = new THREE.AmbientLight('#fff', 1);
+    scene.add(light);
+  }
+
+  const texture = loader.load(world);
+  const material = new THREE.MeshStandardMaterial({
+    map: texture,
+    color: '#fff',
+    wireframe: true,
+  });
+  const sphereGeo = new THREE.SphereGeometry(1, 64, 32);
+  const sphere = new THREE.Mesh(sphereGeo, material);
+  sphere.position.set(0, 5, 0);
+
+  scene.add(sphere);
+
+  controls.target = sphere.position;
+};
 
 function App() {
   const { ref } = useThree({ init: initThree });
