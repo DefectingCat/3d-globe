b674ff20625e16bf5171fc591568d9df0b148ac9
DefectingCat <i@defect.ink>
2022-09-29 16:49:37 +0800

    update hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import routes from 'routes';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={createBrowserRouter(routes)} />
+    <RouterProvider router={createHashRouter(routes)} />
   </React.StrictMode>
 );
